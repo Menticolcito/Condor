a1a9681096bbf2de99c63be82c4a99e6595e8032
Rubén Melo <rbmcar2@gmail.com>
2018-05-05 22:18:37 -0500

    Setup Backend ready

--- SQL/Report3.sql
+++ SQL/Report3.sql
@@ -2,4 +2,4 @@ SELECT  COUNT(DISTINCT user_profile.id_user) AS "Non-active​ ​Providers"
 FROM    user_profile, user_role 
 WHERE   user_profile.id_user = user_role.id_user 
         AND user_role.cd_role_type = "PROVIDER" 
-        AND user_role.in_status != 1 
+        AND user_role.in_status != 1
